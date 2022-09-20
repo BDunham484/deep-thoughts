f1501bceafee979a14095781a340ff080c044a5c
ThereWolf <bdunham484@gmail.com>
2022-09-20 13:28:54 -0500

    add token

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,21 @@
 const { User, Thought } = require('../models');
 //import gql error handling
 const { AuthenticationError } = require('apollo-server-express');
+//import signToken() from utilities
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('thoughts')
+                    .populate('friends');
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
         thoughts: async (parent, { username }) => {
             const params = username ? { username } : {};
             return Thought.find(params).sort({ createdAt: -1 });
@@ -13,21 +25,22 @@ const resolvers = {
         },
         users: async () => {
             return User.find()
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
         },
         user: async (parent, { username }) => {
             return User.findOne({ username })
-            .select('-__v -password')
-            .populate('friends')
-            .populate('thoughts');
+                .select('-__v -password')
+                .populate('friends')
+                .populate('thoughts');
         }
     },
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
-            return user;
+            const token = signToken(user);
+            return { token, user };
         },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
@@ -38,7 +51,8 @@ const resolvers = {
             if (!correctPw) {
                 throw new AuthenticationError('Incorrect credentials');
             }
-            return user;
+            const token = signToken(user);
+            return { token, user };
         }
     }
 };
--- server/server.js
+++ server/server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 //import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
-
 //import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+//import database connection
 const db = require('./config/connection');
+//import auth middleware
+const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
 //creat e new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
